d63394429e31a992666f5e61e8d642eed9ebfcfe
yilayd <yilayda94@gmail.com>
2024-12-16 14:50:06 +0000

    package

--- dags/spacedev_xcom.py
+++ dags/spacedev_xcom.py
@@ -3,7 +3,7 @@ from airflow.models import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
-from airflow.operators.taskinstance import TaskInstance as ti
+from airflow.models.taskinstance import TaskInstance as ti
 
 
 API_URL = "https://lldev.thespacedevs.com/2.3.0/launches"
