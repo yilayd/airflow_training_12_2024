04e7e7c035c6d7b6cacd4f2fbca9aa1591f579e0
yilayd <yilayda94@gmail.com>
2024-12-16 14:08:12 +0000

    nothing

--- dags/sensors.py
+++ dags/sensors.py
@@ -14,6 +14,6 @@ with DAG(
 
     check_data = HttpSensor(task_id = 'check_data', http_conn_id = 'http_delayed', mode = 'reschedule', endpoint = '')
 
-    run_something = PythonOperator(task_id = 'doing_stuff', python_callable = lambda x: print('doing it for'))
+    run_something = PythonOperator(task_id = 'doing_stuff', python_callable = lambda: print('doing it for'))
 
     check_data >> run_something
