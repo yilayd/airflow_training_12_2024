2fba10b4100c98f96a3d6c255b909b69e4a997fb
yilayd <yilayda94@gmail.com>
2024-12-16 12:26:15 +0000

    scheduler trial

---
+++ dags/scheduling_exercise_every3days.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+
+
+with DAG(
+    dag_id = 'launch_rocket_scheduler',
+    start_date = datetime.now() - timedelta(days=90),
+    description = "Empty operators",
+    schedule = timedelta(days=3),
+):
+
+    rocket_material = EmptyOperator(task_id = 'procure_rocket_material') 
+    fuel = EmptyOperator(task_id = 'procure_fuel') 
+
+    stages  = [EmptyOperator(task_id = f"building_stage_{i}") for i in range(1,4)]
+    
+    launch = EmptyOperator(task_id = 'launching')
+
+    rocket_material >> stages >> launch
+
+    fuel >> stages[-1] >> launch
+
