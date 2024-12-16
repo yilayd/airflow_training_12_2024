f8f596b5a9387352ef6eecd28cb5ef75a36dcd01
yilayd <yilayda94@gmail.com>
2024-12-16 10:32:15 +0000

    launch rocket dag

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -14,3 +14,16 @@ with DAG(
     world = PythonOperator(task_id = "world", python_callable = lambda: print("world"))
 
     hello >> world
+
+# 2nd way
+# my_dag =  DAG(
+#     dag_id = 'hello_world',
+#     start_date = datetime.now() - timedelta(days=14),
+#     description = 'This DAG will print Hello and World',
+#     schedule = "@daily",
+# ):
+
+#     hello = BashOperator(task_id = 'hello', bash_command = "echo 'hello'", dag= my_dag)
+#     world = PythonOperator(task_id = "world", python_callable = lambda: print("world") dag= my_dag)
+
+#     hello >> world
---
+++ dags/launch_rocket.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id = 'launch_rocket',
+    start_nate = datetime.now - timedelta(days=1),
+    description = "Empty operators",
+    schedule = '@daily'
+)
+
+    procure_rocket_material = EmptyOperator(task_id = 'Rocket material is procured') 
+    
+    procure_fuel = EmptyOperator(task_id = 'Rocket is fueled') 
+
+    build_stage_1 = EmptyOperator(task_id = 'Building stage 1') 
+
+    build_stage_2 = EmptyOperator(task_id = 'Building stage 2') 
+
+    build_stage_3 = EmptyOperator(task_id = 'Building stage 3') 
+
+    launch = EmptyOperator(task_id = 'Ready to launch!')
+
+
+    procure_rocket_material >> [build_stage_1, build_stage_2, build_stage_3] >> launch
+
+    procure_fuel >> build_stage_3 >> launch
+
