206e36a7d307399ef35d0bcca1e0423c74d7b4c8
yilayd <yilayda94@gmail.com>
2024-12-17 08:35:48 +0000

    print

--- dags/context_exercise.py
+++ dags/context_exercise.py
@@ -11,50 +11,58 @@ from pprint import pprint
 
 API_URL = "https://lldev.thespacedevs.com/2.3.0/launches"
 
-def _download_launches(**context):
-    templates_dict = context["templates_dict"]
-    output_path = Path(templates_dict["output_path"])
+with DAG(
+    dag_id="exercise_templating",
+    start_date=airflow.utils.dates.days_ago(7),
+    default_args=default_args,
+    schedule_interval="@daily",
+) as dag:
 
-    response = requests.get(
-        API_URL,
-        params={
-            "window_start__gte": templates_dict["window_start"],
-            "window_end__lt": templates_dict["window_end"],
+    def _download_launches(**context):
+        templates_dict = context["templates_dict"]
+        output_path = Path(templates_dict["output_path"])
+
+        response = requests.get(
+            API_URL,
+            params={
+                "window_start__gte": templates_dict["window_start"],
+                "window_end__lt": templates_dict["window_end"],
+            },
+        )
+        response.raise_for_status()
+
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+        with output_path.open("w") as file_:
+            json.dump(response.json(), file_)
+
+    def _print_launch_count(**context):
+        input_path = context["templates_dict"]["input_path"]
+
+        with Path(input_path).open() as file_:
+            launches = json.load(file_)
+
+        print(f"""Counted {launches["count"]} launches from {input_path}""")
+
+    print_date = BashOperator(
+        task_id="print_date", bash_command="echo {{ logical_date }}"
+    )
+
+    download_launches = PythonOperator(
+        task_id="download_launches",
+        python_callable=_download_launches,
+        templates_dict={
+            "output_path": "/tmp/launches/{{ds}}.json",
+            "window_start": "{{data_interval_start | ds}}T00:00:00Z",
+            "window_end": "{{data_interval_end | ds}}T00:00:00Z",
         },
     )
-    response.raise_for_status()
 
-    output_path.parent.mkdir(parents=True, exist_ok=True)
-    with output_path.open("w") as file_:
-        json.dump(response.json(), file_)
+    check_for_launches = PythonOperator(
+        task_id="check_for_launches",
+        python_callable=_print_launch_count,
+        templates_dict={"input_path": "/tmp/launches/{{ds}}.json"},
+    )
 
-
-def _print_launch_count(**context):
-    # TODO: Finish this task. Should load the launch JSON file
-    # and print the 'count' field from it's contents.
-
-    input_path = context['templates_dict']['input_path']
-
-    with input_path.open("w") as file_:
-        launches = json.load(file_)
-
-    print('Number of launch is ', launches['count'], "from", input_path )
-
-def print_context_func(numbers, **context):
-    print(numbers[0])
-    pprint(context)
-
-with DAG(
-    dag_id="context_exercise",
-    start_date=airflow.utils.dates.days_ago(7),
-    schedule_interval="@daily",
-):
-
-    echo_logical_date = BashOperator(task_id = 'print_logical_date', bash_command = "echo {{ ts }}")
-
-    launch_data = PythonOperator(task_id = 'fetch_launch_data', python_callable = _download_launches, templates_dict = {"output_path": "/tmp/launches/2021-01-01.json",
-            "window_start": "2021-01-01T00:00:00Z",
-            "window_end": "2021-01-02T00:00:00Z",})
 
     launch_count = PythonOperator(task_id = 'count_launch', python_callable = _print_launch_count, templates_dict = {"input_path": "/tmp/launches/2021-01-02.json"})
     print_context = PythonOperator(
